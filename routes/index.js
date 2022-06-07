54a824fc7fc4b87f3ca77c517ddb0911ffa433e0
Bittu Kumar <bittuk85btechcse2024@kccitm.edu.in>
2022-06-07 11:34:35 +0530

    get_and_post

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('student', { title: 'Express' });
+  
+  res.render('index', { title: 'Express' });
 });
 
+router.get('/data', function(req, res, next) {
+  res.json({name:req.query.name,age:req.query.age,country:req.query.country});
+});
+router.post('/about', function(req, res, next) {
+  // res.send("hello");
+  res.json({name:req.body.name,age:req.body.age,country:req.body.country});
+});
 module.exports = router;
